363cc51a4d4b48f54fcc2d2e7dc1959dcb479cf8
MadanMohanReddy <madan.patakota@hotmail.com>
2021-02-27 18:38:39 +0530

    ngTemplateOutlet Branch has created-Init

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,17 +1,35 @@
-import {
-  Component
-} from '@angular/core';
-
-import { OrdersComponent } from '../orders/orders.component';
-import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component, OnInit, Input, TemplateRef, ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
 
+  messagePromise: Promise<string>;
+  //CustomerName: Subscription;
+  ngOnInit() {
+    this.messagePromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("MadanMohan");
+      }, 3000);
+    });
+  }
+  GetCustomer() {
+    this.messagePromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Madan Mohan Reddy");
+      }, 2000);
+    });
+
+  }
+
+  @Input('ordersTemplate') ordersTemplateInputRef:TemplateRef<any>;
+  @ContentChild('contentCustomer') 
+  contentCustomerRef:TemplateRef<any>;
+
+  
 
 
 }
